45949d11658af64615611cf4f5097d6a81d58933
Travor Liu <travor_lzh@outlook.com>
2019-07-04 03:19:45 +0800

    New scene - LimitScene is implemented
    Now the video also visualizes the process of finding the limit of a
    function
    
    * list.txt: Add the scene to the playlist
    * scenes.py: Implementation

--- scenes.py
+++ scenes.py
@@ -5,14 +5,14 @@ import numpy as np
 class Openning(Scene):
     def construct(self):
         title=TextMobject("Collections of Calculus knowledge",
-                tex_to_color_map={
-                    "Collections":GREEN,
-                    "Calculus":BLUE
-                })
+            tex_to_color_map={
+                "Collections":GREEN,
+                "Calculus":BLUE
+            })
         comment=TextMobject("By @TravorLZH",
-                tex_to_color_map={
-                    "@TravorLZH":YELLOW
-                })
+            tex_to_color_map={
+                "@TravorLZH":YELLOW
+            })
         VGroup(comment,title).arrange(UP)
         self.play(ShowCreation(title))
         self.wait()
@@ -23,74 +23,279 @@ class Openning(Scene):
 
 class ShowFormula(Scene):
     def construct(self):
-        formula=TexMobject("y=\\sin(x)")
-        formula2=TexMobject("\\frac{dy}{dx}=\\frac{d}{dx}[\\sin(x)]",
-                tex_to_color_map={
-                    "\\frac{dy}{dx}":YELLOW,
-                    "\\frac{d}{dx}[":YELLOW,
-                    "]":YELLOW
-                    })
-        formula3=TexMobject("\\frac{dy}{dx}=\\cos(x)",
+        func_formula=TexMobject("y=\\sin(x)")
+        func_comment=TextMobject("Original Function sin(x)",
+            tex_to_color_map={
+                "sin":GREEN
+            })
+        deriv_formula=TexMobject("\\frac{dy}{dx}=\\frac{d}{dx}[\\sin(x)]",
+            tex_to_color_map={
+                "\\frac{dy}{dx}":YELLOW,
+                "\\frac{d}{dx}[":YELLOW,
+                "]":YELLOW
+            })
+        formulas=[
+            TexMobject("\\frac{dy}{dx}=\\cos(x)",
                 tex_to_color_map={
                     "\\frac{dy}{dx}":YELLOW,
                     "\\cos":BLUE
-                })
-        formula4=TexMobject("\\frac{d^2y}{dx^2}=-\\sin(x)",
+                }),
+            TexMobject("\\frac{d^2y}{dx^2}=-\\sin(x)",
                 tex_to_color_map={
                     "\\frac{d^2y}{dx^2}":RED,
                     "-\\sin":GREEN
+                }),
+            TexMobject("\\frac{d^3y}{dx^3}=-\\cos(x)",
+                tex_to_color_map={
+                    "\\frac{d^3y}{dx^3}":ORANGE,
+                    "-\\cos":BLUE
+                }),
+            TexMobject("\\frac{d^4y}{dx^4}=\\sin(x)",
+                tex_to_color_map={
+                    "\\frac{d^4y}{dx^4}":GREEN
                 })
-        self.play(Write(formula))
+        ]
+        comments=[
+            TextMobject("First derivative of sin(x)",
+                tex_to_color_map={
+                    "First":YELLOW,
+                    "sin":PURPLE
+                }),
+            TextMobject("Second derivative of sin(x)",
+                tex_to_color_map={
+                    "Second":RED,
+                    "sin":PURPLE
+                }),
+            TextMobject("Third derivative of sin(x)",
+                tex_to_color_map={
+                    "Third":ORANGE,
+                    "sin":PURPLE
+                }),
+            TextMobject("Fourth derivative of sin(x)",
+                tex_to_color_map={
+                    "Fourth":GREEN,
+                    "sin":PURPLE
+                })
+        ]
+        VGroup(func_formula,func_comment).arrange(UP)
+        self.play(FadeIn(func_comment),Write(func_formula))
         self.wait()
-        self.play(Transform(formula,formula2))
-        self.wait()
-        self.play(Transform(formula,formula3))
-        self.wait()
-        self.play(Transform(formula,formula4))
-        self.wait()
-        self.play(FadeOut(formula))
+        self.play(FadeOut(func_comment),Transform(func_formula,deriv_formula))
+        for formula,comment in zip(formulas,comments):
+            VGroup(formula,comment).arrange(UP)
+            self.play(Transform(func_formula,formula),
+                Transform(func_comment,comment))
+        self.play(FadeOut(func_comment),FadeOut(func_formula))
         self.wait()
 
 class GraphStuff(GraphScene):
     CONFIG={
-        "x_min": -10,
-        "x_max": 10,
-        "y_min": -10,
-        "y_max": 10,
+        "x_min": -8,
+        "x_max": 8,
+        "y_min": -5,
+        "y_max": 5,
         "graph_origin": ORIGIN,
     }
 
-    def tangent_line(self,x):
-        return np.cos(3)*(x-3)+np.sin(3)
+    @staticmethod
+    def tangent_line(x):
+        return np.cos(np.pi)*(x-np.pi)+np.sin(np.pi)
 
-    def tangent_line2(self,x):
-        return -np.sin(3)*(x-3)+np.cos(3)
+    @staticmethod
+    def tangent_line2(x):
+        return -np.sin(np.pi)*(x-np.pi)+np.cos(np.pi)
+
+    @staticmethod
+    def tangent_line3(x):
+        return -np.cos(np.pi)*(x-np.pi)-np.sin(np.pi)
+
+    @staticmethod
+    def tangent_line4(x):
+        return np.sin(np.pi)*(x-np.pi)-np.cos(np.pi)
+
+    @staticmethod
+    def tangent_line5(x):
+        return np.exp(1)*(x-1)+np.exp(1)
+
+    @staticmethod
+    def negate_sin(x):
+        return -np.sin(x)
+    
+    @staticmethod
+    def negate_cos(x):
+        return -np.cos(x)
 
     def construct(self):
         self.setup_axes(animate=True)
-        graph=self.get_graph(np.sin)
+        graph=self.get_graph(np.sin,color=GREEN)
         label=self.get_graph_label(graph,label="\\sin(x)")
         self.play(ShowCreation(graph),Write(label))
-        pt=Dot(self.coords_to_point(3,np.sin(3)))
-        tangentline=self.get_graph(self.tangent_line)
-        self.play(ShowCreation(tangentline))
-        self.play(ShowCreation(pt))
+        pt=Dot(self.coords_to_point(np.pi,np.sin(np.pi)))
+        tangentline=self.get_graph(self.tangent_line,color=YELLOW)
+        self.play(ShowCreation(tangentline),ShowCreation(pt))
         self.wait()
         # Now change sin(x) to cos(x) and the tangent line respectively
-        graph2=self.get_graph(np.cos)
+        graph2=self.get_graph(np.cos,color=BLUE)
         label2=self.get_graph_label(graph2,"\\cos(x)")
+        pt2=Dot(self.coords_to_point(np.pi,np.cos(np.pi)))
+        tangentline2=self.get_graph(self.tangent_line2,color=YELLOW)
         self.wait()
-        pt2=Dot(self.coords_to_point(3,np.cos(3)))
-        tangentline2=self.get_graph(self.tangent_line2)
         self.play(Transform(graph,graph2),Transform(label,label2),
-                Transform(pt,pt2),Transform(tangentline,tangentline2))
-        poweredby=TextMobject("Powered by \\LaTeX\\ and manim",
-                tex_to_color_map={"manim":BLUE})
-        author=TextMobject("Created by @TravorLZH",
-                tex_to_color_map={"@TravorLZH":YELLOW})
+            Transform(pt,pt2),Transform(tangentline,tangentline2))
+        # Now to -sin(x)
+        graph3=self.get_graph(self.negate_sin,color=GREEN)
+        label3=self.get_graph_label(graph3,"-\\sin(x)")
+        pt3=Dot(self.coords_to_point(np.pi,-np.sin(np.pi)))
+        tangentline3=self.get_graph(self.tangent_line3,color=YELLOW)
         self.wait()
-        VGroup(poweredby,author).arrange(UP)
+        self.play(Transform(graph,graph3),Transform(label,label3),
+            Transform(pt,pt3),Transform(tangentline,tangentline3))
+        # Now to -cos(x)
+        graph4=self.get_graph(self.negate_cos,color=BLUE)
+        label4=self.get_graph_label(graph4,"-\\cos(x)")
+        pt4=Dot(self.coords_to_point(np.pi,-np.cos(np.pi)))
+        tangentline4=self.get_graph(self.tangent_line4,color=YELLOW)
+        self.wait()
+        self.play(Transform(graph,graph4),Transform(label,label4),
+            Transform(pt,pt4),Transform(tangentline,tangentline4))
+        self.wait()
+        graph5=self.get_graph(np.exp,color=GREEN)
+        label5=self.get_graph_label(graph5,"e^x")
+        pt5=Dot(self.coords_to_point(1,np.exp(1)))
+        tangentline5=self.get_graph(self.tangent_line5,color=YELLOW)
+        self.wait()
+        self.play(Transform(graph,graph5),Transform(label,label5),
+            Transform(pt,pt5),Transform(tangentline,tangentline5))
+        self.wait()
         self.play(FadeOut(tangentline),FadeOut(graph),
-                FadeOut(label),FadeOut(self.axes),FadeOut(pt))
+            FadeOut(label),FadeOut(self.axes),FadeOut(pt))
+
+class LimitScene(GraphScene):
+    CONFIG={
+        "x_min": -2,
+        "x_max": 1,
+        "y_min": -1,
+        "y_max": 4,
+        "graph_origin": 2*DOWN,
+    }
+
+    @staticmethod
+    def func(x):
+        return (1+x**3)/(1+x)
+
+    def construct(self):
+        title=TextMobject(
+                "Limit of $\\frac{x^3+1}{x+1}$ at $x=-1$")
+        formula="\\frac{(%f)^3+1}{%f+1}= %f"
+        y=self.func(-0.9)
+        str_y="%f" % y
+        formula_orig=TexMobject(formula % (-0.9,-0.9,y),
+            tex_to_color_map={str_y:RED})
+        VGroup(formula_orig,title).arrange(UP)
+        self.play(Write(title))
+        self.play(ShowCreation(formula_orig))
+        dx=0.01
+        formatted=""
+        for i in range(5):
+            self.wait()
+            x=-1.0+dx
+            y=self.func(x)
+            str_y="%f" % y
+            formatted=formula % (x,x,y)
+            formula_obj=TexMobject(formatted,
+                tex_to_color_map={str_y:RED})
+            VGroup(formula_obj,title).arrange(UP)
+            self.play(Transform(formula_orig,formula_obj))
+            dx/=10
+        self.wait()
+        to_append="\\approx 3"
+        formula_approx=TexMobject(formatted+to_append,
+            tex_to_color_map={str_y:RED,to_append:YELLOW})
+        VGroup(formula_approx,title).arrange(UP)
+        self.play(Transform(formula_orig,formula_approx))
+        self.wait()
+        newtitle=TextMobject(
+            "Limit of $f(x)$ at $x=-1$",
+            tex_to_color_map={"$f(x)$":BLUE}).to_edge(UP).to_edge(RIGHT)
+        self.play(Transform(title,newtitle),FadeOut(formula_orig))
+        self.setup_axes(animate=True)
+        func_graph=self.get_graph(self.func,color=BLUE)
+        func_lbl=self.get_graph_label(func_graph,
+                "f(x)=\\frac{x^3+1}{x+1} \\ (x \\ne -1)").to_edge(DOWN)
+        self.play(ShowCreation(func_graph),ShowCreation(func_lbl))
+        newformula="f(%f)=%f"
+        y=self.func(-0.9)
+        str_x="%f" % -0.9
+        str_y="%f" % y
+        newformula_orig=TexMobject(newformula
+            % (-0.9,y),
+            tex_to_color_map={
+                str_x:GREEN,
+                str_y:RED
+            }).to_edge(RIGHT)
+        pt_orig=Dot(self.coords_to_point(-0.9,y))
+        vline_orig=self.get_vertical_line_to_graph(-0.9,func_graph,
+                color=GREEN)
+        a=self.coords_to_point(0,y)
+        b=self.coords_to_point(-0.9,y)
+        hline_orig=Line(a,b,color=RED)
+        self.play(Write(newformula_orig),ShowCreation(vline_orig),
+                ShowCreation(hline_orig),ShowCreation(pt_orig))
+        dx=0.01
+        for i in range(5):
+            self.wait()
+            x=-1.0+dx
+            y=self.func(x)
+            str_x="%f" % x
+            str_y="%f" % y
+            formatted=newformula % (x,y)
+            pt_obj=Dot(self.coords_to_point(x,y))
+            vline_obj=self.get_vertical_line_to_graph(x,func_graph,
+                    color=GREEN)
+            a=self.coords_to_point(0,y)
+            b=self.coords_to_point(x,y)
+            hline_obj=Line(a,b,color=RED)
+            newformula_obj=TexMobject(formatted,
+                tex_to_color_map={
+                    str_x:GREEN,
+                    str_y:RED
+                }).to_edge(RIGHT)
+            self.play(Transform(newformula_orig,newformula_obj),
+                    Transform(vline_orig,vline_obj),
+                    Transform(hline_orig,hline_obj),Transform(pt_orig,pt_obj))
+            dx/=10
+        self.wait()
+        newformula_approx=TexMobject(formatted+to_append,
+            tex_to_color_map={
+                str_x:GREEN,
+                str_y:RED,
+                to_append:YELLOW
+            }).to_edge(RIGHT)
+        self.play(Transform(newformula_orig,newformula_approx))
+        self.wait()
+        newformula_lim=TexMobject("\\lim_{x \\to -1}f(x)=3",
+            tex_to_color_map={
+                "_{x \\to -1}":GREEN,
+                "f(x)":BLUE,
+                "3":YELLOW
+            }).to_edge(RIGHT)
+        self.play(Transform(newformula_orig,newformula_lim))
+        self.wait()
+        self.play(FadeOut(func_graph),FadeOut(func_lbl),FadeOut(title),
+            FadeOut(newformula_orig),FadeOut(pt_orig),FadeOut(hline_orig),
+            FadeOut(vline_orig),FadeOut(self.axes))
+        self.wait()
+        summary=TexMobject("\\therefore \\lim_{x \\to -1}\\frac{x^3+1}{x+1}=3")
+        self.play(Write(summary))
+        self.wait()
+        self.play(FadeOut(summary))
+
+class ThanksScene(Scene):
+    def construct(self):
+        poweredby=TextMobject("Powered by \\LaTeX\\ and manim",
+            tex_to_color_map={"manim":BLUE})
+        author=TextMobject("Created by @TravorLZH",
+            tex_to_color_map={"@TravorLZH":YELLOW})
+        VGroup(poweredby,author).arrange(UP)
         self.play(FadeIn(poweredby),Write(author))
         self.wait()
